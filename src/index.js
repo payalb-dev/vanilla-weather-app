44acf2af7c6fbcb35e3e69864b9f06210931ced6
Payal Bansal <payalbansal2502@gmail.com>
2022-12-07 04:37:09 +0530

    update date & small change to css in temperature

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+function formatDate(timestamp){
+  //calulate date in milisecond i.e. timestamp
+  let date =new Date(timestamp)
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let day = date.getDay();
+  return `${day}${hours}:${minutes}`;
+
+}
+
+
+
 function displayTemperature(response){
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -5,11 +17,14 @@ function displayTemperature(response){
       let descriptionElement =document.querySelector("#description");
       let humidityElement =document.querySelector("#humidity");
       let windElement =document.querySelector("#wind");
+      let dateElement = document.querySelector("#date");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML =response.data.name;
      descriptionElement.innerHTML =response.data.weather[0].description;
      humidityElement.innerHTML =response.data.main.humidity;
      windElement.innerHTML = Math.round(response.data.wind.speed);
+     dateElement.innerHTML = formatDate(response.data.dt*1000) ;
+    
 }
 
 let apiKey ="99b8f9330a1bfba3a85e523fd3c2e528";
