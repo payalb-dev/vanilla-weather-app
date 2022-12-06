90998dcfacd43437512f8a00b552dbd801d9d3c3
Payal Bansal <payalbansal2502@gmail.com>
2022-12-07 04:56:23 +0530

    updated time

--- src/index.js
+++ src/index.js
@@ -2,8 +2,21 @@ function formatDate(timestamp){
   //calulate date in milisecond i.e. timestamp
   let date =new Date(timestamp)
   let hours = date.getHours();
+  if (hours<10){
+    hours =`0${hours}`
+  }
   let minutes = date.getMinutes();
-  let day = date.getDay();
+  if (minutes<10){
+    minutes =`0${minutes}`
+  }
+  let days =["Sunday",
+ "Monday",
+"Tuesday",
+"Wednesday",
+"Thursday",
+"Friday",
+"Saturday"];
+let day = days[date.getDay()];
   return `${day}, ${hours}:${minutes}`;
 
 }
@@ -28,6 +41,7 @@ function displayTemperature(response){
 }
 
 let apiKey ="99b8f9330a1bfba3a85e523fd3c2e528";
+let city
 let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=Lisbon&appid=${apiKey}&units=metric`;
 console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
