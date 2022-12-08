ed97d702831a6c9cb2c3fc17dc17903c6c24bbf3
Payal Bansal <payalbansal2502@gmail.com>
2022-12-08 10:05:53 +0530

    working on units

--- src/index.js
+++ src/index.js
@@ -40,22 +40,31 @@ function displayTemperature(response){
      dateElement.innerHTML = formatDate(response.data.dt*1000) ;
      iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
      //image is broken use alterative with description
-     iconElement.setAttribute("alt",response.data.weather[0].description)
+     iconElement.setAttribute("alt",response.data.weather[0].description);
     
 }
 function search(city){
 let apiKey ="99b8f9330a1bfba3a85e523fd3c2e528";
 let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
 
 }
 
 function handleSubmit(event){
 event.preventDefault();
-let cityInputElement =document.querySelector("#city-input")
+let cityInputElement = document.querySelector("#city-input");
 search(cityInputElement.value);
 }
-search("Lisbon")
-let form =document.querySelector("#search-form")
-form.addEventListener("submit",search)
+function displayFahrenheitTemperature(event){
+  event.preventDefault();
+   let temperatureElement = document.querySelector("#temperature");
+  let fahrenheitTemperature = (temperatureElement.innerHTML * 9)/5 + 32 ;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit",handleSubmit);
+search("Lisbon");
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click",displayFahrenheitTemperature);
